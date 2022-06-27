169b90a8be8f28b3e09ba400584819be040c3839
aanandisupriya584 <88267165+aanandisupriya584@users.noreply.github.com>
2022-06-27 21:34:14 +0530

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,68 @@
+/*
+ Problem Statement
+Given an array A of size N, you need to find its maximum, 2nd maximum and 3rd maximum element.
+
+Try solving it in O(N) per test case
+Input
+The first line of the input contains the number of test cases T.
+
+For each test case, the first line of the input contains an integer N denoting the number of elements in the array A. The next line contains N (space separated) elements of A.
+
+
+Constraints:
+1 <= T <= 100
+3 <= N <= 10^6
+1 <= A[i] <= 10^9
+
+Note:-It is guaranteed that the sum of N over all text cases does not exceed 10^6
+Output
+For each test case, output the first, second and third maximum elements in the array.
+Example
+Sample Input:
+3
+5
+1 4 2 4 5
+6
+1 3 5 7 9 8
+7
+11 22 33 44 55 66 77
+
+Sample Output:
+5 4 4
+9 8 7
+77 66 55
+
+Explanation(might now be the optimal solution):
+Testcase 1:
+[1 4 2 4 5]
+First max = 5
+Second max = 4
+Third max = 4*/
+package arrayPrc;
+import java.io.*;
+import java.util.*;
+public class Maximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+		
+		while(T>0)
+		  {
+          int N = sc.nextInt();
+			int a[]=new int[N];
+
+			for(int i=0;i<N;i++)
+			{
+           a[i]=sc.nextInt();
+			}
+			Arrays.sort(a);
+			System.out.println(a[N-1]+" "+a[N-2]+" "+a[N-3]);
+			T--;
+		  }
+		  }
+
+	}
+
+
